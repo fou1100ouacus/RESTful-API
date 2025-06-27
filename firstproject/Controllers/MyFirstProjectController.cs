439d0e50c392d6ae9b2f015d85ce269553e479d4
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-06-27 20:55:33 +0300

    Add files via upload

---
+++ firstproject/Controllers/MyFirstProjectController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstproject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyFirstProjectController : ControllerBase
+    {
+        [HttpGet]
+        public string GetMyName()
+        {
+            return "My name is aya";
+        }
+        [HttpGet("n1,nk2")]
+        public int sum(int n1,int n2)
+        {
+            return n1 + n2;
+
+        }
+
+
+    
+    }
+}
